dd6752af25b3d9cbdf6421dbd58da3941c83cb8a
Ziana <zianaodero@gmail.com>
2024-09-01 12:54:01 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -95,7 +95,7 @@ st.write("Fill the form below to get your course recommendations:")
 # Input Form
 with st.form(key='recommendation_form'):
     gender = st.selectbox("Gender", ["Select Gender", "Male", "Female"])
-    extracurricular_activities = st.checkbox("Involved in Extracurricular Activities")
+    extracurricular_activities = st.checkbox(" Check box if you were involved in Extracurricular Activities in highschool")
     riasec = st.slider("RIASEC Score", min_value=1, max_value=40, value=20)
     math_score = st.number_input("Math Score", min_value=0, max_value=100)
     history_score = st.number_input("History Score", min_value=0, max_value=100)
